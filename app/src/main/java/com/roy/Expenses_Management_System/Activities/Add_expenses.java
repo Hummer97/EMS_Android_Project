3149b353b63e4a7995a47d7c1d35f02128924bcc
TheHummer <avinashroy.it@gmail.com>
2020-05-19 02:22:01 +0530

    Add_expenses UI work

--- app/src/main/java/com/roy/Expenses_Management_System/Activities/Add_expenses.java
+++ app/src/main/java/com/roy/Expenses_Management_System/Activities/Add_expenses.java
@@ -14,29 +14,5 @@ public class Add_expenses extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_expenses);
-
-        final EditText Item = (EditText) findViewById(R.id.itemDis);
-        final EditText Price = (EditText) findViewById(R.id.itemPrice);
-        Button ShowData = (Button) findViewById(R.id.showData);
-//        boolean d=true;
-//        if(d == true)
-//        {
-//            ShowData.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//                    Toast.makeText(this,"Data Submitted Successfully",Toast.LENGTH_LONG).show();
-//                }
-//            });
-//        }
-
-//        ShowData.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                String Itemdata = Item.getText().toString();
-//                String ItemPrice = Price.getText().toString();
-//                ShowItem.setText(Itemdata);
-//                ShowPrice.setText(ItemPrice);
-//            }
-//        });
     }
 }
